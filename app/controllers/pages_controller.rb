e8c86b1dcbffa3aef1538ec55fdead7957c6eb1d
Jash Shah <jash.shah@live.com>
2019-08-23 20:26:02 +0530

    Add more design to Home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,12 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  # def mobile_device?
+  #   if session[:mobile_param]
+  #     session[:mobile_param] == "1"
+  #   else
+  #     request.user_agent =~ /Mobile|webOS/
+  #   end
+  # end
 end
