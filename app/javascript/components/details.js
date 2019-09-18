6f94858c09a24f03238ddb9bee6f221830113207
Jash Shah <jash.shah@live.com>
2019-09-18 23:05:26 +0530

    Add sectors and improve design

--- app/javascript/components/details.js
+++ app/javascript/components/details.js
@@ -1,7 +1,6 @@
 const details = document.querySelectorAll(".detail-text");
 
 details.forEach(detail => {
-  console.log(detail);
   detail.addEventListener('click', () => {
     detail.childNodes[3].classList.toggle("hidden")
     console.log(detail);
