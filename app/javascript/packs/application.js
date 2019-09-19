5c6c2f7f2a40eee21f7810d99e93d5df354d022a
Jash Shah <jash.shah@live.com>
2019-09-19 14:24:27 +0530

    Fix navbar, add colour to bottom

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,9 @@ import { showSlides } from '../components/image';
 import { currentSlides } from '../components/image';
 import { loadDynamicBannerText } from '../components/banner';
 import { details } from '../components/details';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 
+initUpdateNavbarOnScroll();
 loadDynamicBannerText();
 details();
 showSlides();
