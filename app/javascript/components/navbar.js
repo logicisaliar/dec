0e9cdd899ed1000962a14c5060380a1cb478f3e0
Jash Shah <jash.shah@live.com>
2019-11-14 20:36:19 +0530

    Remove navbar js

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,16 +1,3 @@
-const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.navbar-lewagon');
-  if (navbar) {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight * 0.3) {
-        navbar.classList.add('navbar-lewagon-white');
-      } else {
-        navbar.classList.remove('navbar-lewagon-white');
-      }
-    });
-  }
-}
-console.log("0");
 const navItems = document.querySelectorAll(".nav-item");
 
 navItems.forEach(navItem => {
@@ -29,4 +16,3 @@ navTogglers.forEach(navToggler => {
 })
 
 
-export { initUpdateNavbarOnScroll };
